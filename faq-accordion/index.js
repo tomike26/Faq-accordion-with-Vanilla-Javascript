de149218a06b9db8d0c895b9524341273a8a78b5
tomike26 <kolajotomike26@gmail.com>
2024-10-02 22:11:06 +0100

    Fix the toggle button on the faq

--- faq-accordion/index.js
+++ faq-accordion/index.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
       let target = e.currentTarget;
       const answer = target.querySelector(".faq-answer");
       const wrapper = target.querySelector(".faq-wrapper");
+      const PlusIcon = target.querySelector(".plus-icon");
+      const MinusIcon = target.querySelector(".minus-icon")
   
       const isExpanded = target.classList.toggle("accordion-expand");
   
@@ -18,13 +20,19 @@ document.addEventListener("DOMContentLoaded", () => {
             item.classList.remove("accordion-expand");
             item.querySelector(".faq-answer").style.display = "none";
             item.querySelector(".faq-wrapper").classList.remove("active");
+            item.querySelector(".plus-icon").style.display="block";
+            item.querySelector(".minus-icon").style.display="none";
           }
         });
         answer.style.display = "block";
         wrapper.classList.add("active");
+        PlusIcon.style.display ="none";
+        MinusIcon.style.display= "block"
       } else {
         answer.style.display = "none";
         wrapper.classList.remove("active");
+          PlusIcon.style.display ="block";
+        MinusIcon.style.display= "none";
       }
     };
   });
